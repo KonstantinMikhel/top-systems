d0818a3fb681141df078ab88ff8c813916ad741c
Konstantin Mikhel <mikhelkonstantin@gmail.com>
2024-02-13 18:00:04 +0300

    Create basic Point and Shape classes

---
+++ include/Point.hpp
@@ -0,0 +1,8 @@
+namespace shapesdraw {
+
+struct Point {
+    double x;
+    double y;
+};
+
+}  // namespace shapesdraw
---
+++ include/Shape.hpp
@@ -0,0 +1,14 @@
+#include "Point.hpp"
+
+namespace shapesdraw {
+
+class Shape {
+protected:
+    Point center;
+
+public:
+    explicit Shape(const Point& c) : center(c) {}
+    virtual void draw() const = 0;
+};
+
+}  // namespace shapesdraw
