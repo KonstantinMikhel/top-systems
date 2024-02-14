547fa3a065f58c573a45295db9e88717ab4e1191
Konstantin Mikhel <mikhelkonstantin@gmail.com>
2024-02-14 21:02:08 +0300

    Refactor Polygon class

--- include/Polygon.hpp
+++ include/Polygon.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <utility>
 
 #include "Shape.hpp"
 #include "Point.hpp"
@@ -12,11 +13,22 @@ protected:
     std::vector<Point> vertices;
 
 public:
-    Polygon() {}
-    Polygon(const std::vector<Point>& vertices);
+    Polygon(std::vector<Point>&& vertices)
+        : vertices{std::move(vertices)}
+    {}
+    
     template<typename... Args>
-    Polygon(Args&&... args);
+    Polygon(Args&&... args)
+        : vertices{std::forward<Args>(args)...}
+    {}
 
+    void draw() const override {
+        glBegin(GL_POLYGON);
+        for (const auto& vertex : vertices) {
+            glVertex2d(vertex.x, vertex.y);
+        }
+        glEnd();  // GL_POLYGON
+    }
 };
 
 }  // shapesdraw 
