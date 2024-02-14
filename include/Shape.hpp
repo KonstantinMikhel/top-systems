a9e424fa886a46760307fff8e389d4f4c094557c
Konstantin Mikhel <mikhelkonstantin@gmail.com>
2024-02-14 21:45:09 +0300

    Add Color property to Shape

--- include/Shape.hpp
+++ include/Shape.hpp
@@ -1,12 +1,24 @@
 #pragma once
 
+#include <iostream>
 #include "Point.hpp"
+#include "Color.hpp"
 
 namespace shapesdraw {
 
 class Shape {
+protected:
+    Color color;
 
 public:
+    Color get_color() const {
+        return color; 
+    }
+
+    void set_color(const Color& clr) {
+        color = clr;
+    }
+     
     virtual void draw() const = 0;
 };
 
